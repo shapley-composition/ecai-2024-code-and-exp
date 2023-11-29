f4c0508508a74326960f14f2c46601dc17d994eb
NOEPG <paul-gauthier.noe@univ-avignon.fr>
2023-11-29 16:20:24 +0100

    3Dplot

--- plot_3D/main2.py
+++ plot_3D/main2.py
@@ -5,11 +5,12 @@ from scipy.spatial import ConvexHull
 
 # This simply creates a set of points:
 
-x = np.array([[0,0,0], [10, -10/sqrt(3), 10], [10, 10/sqrt(2), 10], [10, 10, 10], [5,5,5], [1, -1/sqrt(3), 3], [0, 10, 0]])
+x = np.array([[0,0,-10], [0, 0, 10],[0, 10, 10] , [0, 10, -10], [10, -10/sqrt(3), -10], [10, -10/sqrt(3), 10], [10, 10, 10], [10, 10, -10]])
 # Then I compute the convex hull using scipy:
 xc = x[ConvexHull(x).vertices]
 
 fig = go.Figure()
+
 fig.add_trace(go.Mesh3d(x=xc[:, 0], 
                         y=xc[:, 1], 
                         z=xc[:, 2], 
@@ -17,16 +18,43 @@ fig.add_trace(go.Mesh3d(x=xc[:, 0],
                         opacity=.5,
                         alphahull=0))
 
-x = np.array([[0,0,0], [10, -10/sqrt(3), 10], [10, 10/sqrt(2), 10], [10, 10, 10], [5,5,5], [1, -1/sqrt(3), 3], [0, 10, 0]])
+
+x = np.array([[0,0,0], [0,0,10], [0, 10, 10], [0, 10, 10/sqrt(2)], [-10, -10/sqrt(3), -10/sqrt(6)], [-10, -10/sqrt(3), 10], [-10, 10, 10], [-10, 10, 10/sqrt(2)]])
+# x = np.array([[0,0,0], [0,0,10], [0, 10, 10], [0, 10, 10/sqrt(2)], [-10, -10/sqrt(3), -10/sqrt(6)], [-10, -10/sqrt(3), 10], [-10, 10, 10], [-10, 10, 10/sqrt(2)]])
 # Then I compute the convex hull using scipy:
 xc = x[ConvexHull(x).vertices]
 
-fig = go.Figure()
 fig.add_trace(go.Mesh3d(x=xc[:, 0], 
                         y=xc[:, 1], 
                         z=xc[:, 2], 
-                        color="blue", 
+                        color="orange", 
                         opacity=.5,
                         alphahull=0))
+
+
+x = np.array([[-10, -10/sqrt(3), -10/sqrt(6)], [-10, -10/sqrt(3), 10], [-10, -10, 10], [-10, -10, -10/sqrt(2)], [0,0,0], [0,0,10], [0,-10, -10/sqrt(2)], [10,-10/sqrt(3),10], [10,-10/sqrt(3),-10/sqrt(6)], [10, -10, -10/sqrt(2)], [10, -10, 10]])
+# Then I compute the convex hull using scipy:
+xc = x[ConvexHull(x).vertices]
+
+fig.add_trace(go.Mesh3d(x=xc[:, 0], 
+                        y=xc[:, 1], 
+                        z=xc[:, 2], 
+                        color="green", 
+                        opacity=.5,
+                        alphahull=0))
+
+
+x = np.array([[10, -10, -10/sqrt(2)], [10, -10, -10] , [-10, -10, -10/sqrt(2)], [-10, -10, -10], [10, 10, -10], [10, 10, 10/sqrt(2)], [-10, 10, -10], [-10, 10, 10/sqrt(2)]])
+# Then I compute the convex hull using scipy:
+xc = x[ConvexHull(x).vertices]
+
+fig.add_trace(go.Mesh3d(x=xc[:, 0], 
+                        y=xc[:, 1], 
+                        z=xc[:, 2], 
+                        color="pink", 
+                        opacity=.5,
+                        alphahull=0))
+
+
 
 fig.show()
