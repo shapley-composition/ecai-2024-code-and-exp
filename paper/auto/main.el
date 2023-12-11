a27d418f2cb55e5cfe69a5f9c829582c447a259d
Paul-Gauthier <paul-gauthier.noe@univ-avignon.fr>
2023-12-11 16:31:55 +0100

    edit paper

--- paper/auto/main.el
+++ paper/auto/main.el
@@ -5,12 +5,12 @@
          "--synctex=1 -shell-escape")
    (TeX-add-to-alist 'LaTeX-provided-package-options
                      '(("cleveref" "capitalize" "noabbrev")))
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "path")
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "url")
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "nolinkurl")
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperbaseurl")
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperimage")
    (add-to-list 'LaTeX-verbatim-macros-with-braces-local "href")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperimage")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperbaseurl")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "nolinkurl")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "url")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "path")
    (add-to-list 'LaTeX-verbatim-macros-with-delims-local "path")
    (TeX-run-style-hooks
     "latex2e"
@@ -39,7 +39,8 @@
     "app:properties"
     "eq:linearsimplex"
     "app:algo"
-    "alg:1")
+    "alg:1"
+    "alg:2")
    (LaTeX-add-bibliographies
     "biblio")
    (LaTeX-add-amsthm-newtheorems
